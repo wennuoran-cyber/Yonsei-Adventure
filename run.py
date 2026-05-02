da9853ea7e90d93d111aede2378bd4f9d50088bf
你的GitHub用户名 <@example.com>
2026-05-03 02:59:18 +0900

    run.py

---
+++ run.py
@@ -0,0 +1,17 @@
+settings = {
+    "난이도": "보통" 
+}
+
+player = {
+    "현재위치": "연대앞 버스정류장",
+    "상태": "배고픔",
+    "HP": 10,  
+    "잔액": 10000 
+}
+
+current_time = "11:00"
+
+print(f"--- 게임 시작 ---")
+print(f"현재 시각: {current_time}")
+print(f"현재 위치: {player['현재위치']}")
+print(f"주인공 상태: {player['상태']} (HP: {player['HP']})")
